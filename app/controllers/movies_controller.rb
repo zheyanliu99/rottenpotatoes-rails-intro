63b1b8bd1c84030a31996a9a4a3ae8e5a0dcd3b6
“Zheyan <zheyan.liu@columbia.edu>
2022-10-01 22:07:25 -0400

    Still some errors in session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,6 +7,8 @@ class MoviesController < ApplicationController
   end
 
   def index
+
+    puts params
     @movies = Movie.with_ratings(params[:ratings])
     @movies = Movie.sort_by_col(params[:sort_by], @movies) if params[:sort_by]
     @sort_by = params[:sort_by]
@@ -14,8 +16,32 @@ class MoviesController < ApplicationController
     # set default to empty
     @ratings_to_show = []
     @ratings_to_show = params[:ratings].keys if params[:ratings]
-    puts(params)
-    # puts params[:ratings]
+    session[:ratings] = params[:ratings].keys if params[:ratings]
+    session[:sort_by] = params[:sort_by] if params[:sort_by]
+    puts(session[:ratings])
+    puts(session[:sort_by])
+
+    puts(@ratings_to_show)
+
+    if not params.has_key?(:ratings) and not params.has_key?(:sort_by) 
+
+      # ratings in session but sort_by is not
+      if session.has_key?(:ratings) and not session.has_key?(:sort_by)
+        puts 'case 1'
+        redirect_to :ratings => session[:ratings].each_with_object({}) { |k, h| h[k] = 1 }
+      # sort_by in session but ratings is not
+      elsif session.has_key?(:sort_by) and not session.has_key?(:ratings)
+        puts 'case 2'
+        redirect_to :sort_by => session[:sort_by]
+      # both in session
+      elsif session.has_key?(:sort_by) and session.has_key?(:ratings)
+        puts 'case 3'
+        redirect_to :ratings => session[:ratings].each_with_object({}) { |k, h| h[k] = 1 }, :sort_by => session[:sort_by]
+      end
+
+    end
+    
+
     # if_empty_key = Movie.with_ratings(params[:ratings])
   end
 
